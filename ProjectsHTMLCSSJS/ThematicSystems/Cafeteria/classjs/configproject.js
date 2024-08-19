beea0e08c4e87b0b34c8425802c78d423c902136
desenvolvedorpaulosergio <marquespaulo7957@gmail.com>
2024-08-19 17:10:04 -0300

    Atualizar configuração JS

--- ProjectsHTMLCSSJS/ThematicSystems/Cafeteria/classjs/configproject.js
+++ ProjectsHTMLCSSJS/ThematicSystems/Cafeteria/classjs/configproject.js
@@ -1,11 +1,14 @@
 function eventoEquiv(){
-    let opcoes = window.document.querySelectorAll('.estilo-opcao-geral');
     let symbols = window.document.querySelectorAll('.material-symbols-outlined');
+    let opcoes = window.document.querySelectorAll('.estilo-opcao-geral');
+    let icone = window.document.getElementById('config-icone');
     opcoes.forEach(function(opcoes){
         if(opcoes.style.display == 'block'){
+            icone.style.display = 'none'
             opcoes.style.display = 'none'
         }else{
             opcoes.style.display = 'block'
+            icone.style.display = 'block'
         }
     })
     symbols.forEach(function(symbols){
@@ -15,4 +18,31 @@ function eventoEquiv(){
             symbols.style.display = 'block'
         }
     })
+}
+
+function configBody(){
+    let symbols = window.document.querySelectorAll('.material-symbols-outlined')
+    let opcoes = window.document.querySelectorAll('.estilo-opcao-geral')
+    let icone = window.document.getElementById('config-icone')
+    let equiv = window.document.getElementById('estilo-equiv')
+
+    if(window.innerWidth > 642){
+        symbols.forEach(function(symbols){
+            symbols.style.display = 'block'
+        })
+        opcoes.forEach(function(opcoes){
+            opcoes.style.display = 'block'
+        })
+        icone.style.display = 'block';
+        equiv.style.display = 'none';
+    }else{
+        symbols.forEach(function(symbols){
+            symbols.style.display = 'none'
+        })
+        opcoes.forEach(function(opcoes){
+            opcoes.style.display = 'none'
+        })
+        icone.style.display = 'none';
+        equiv.style.display = 'block';
+    }
 }
