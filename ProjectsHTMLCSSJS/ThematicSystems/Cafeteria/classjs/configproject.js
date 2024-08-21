5e5b21716ad87ed4ca12b88b7b6ebfea0cd8ecc1
desenvolvedorpaulosergio <marquespaulo7957@gmail.com>
2024-08-21 17:03:58 -0300

    Atualizar erro

--- ProjectsHTMLCSSJS/ThematicSystems/Cafeteria/classjs/configproject.js
+++ ProjectsHTMLCSSJS/ThematicSystems/Cafeteria/classjs/configproject.js
@@ -1,5 +1,5 @@
 function eventoEquiv(){
-    let symbols = window.document.querySelectorAll('.material-symbols-outlined');
+    let symbols = window.document.querySelectorAll('.material-symbols-display');
     let opcoes = window.document.querySelectorAll('.estilo-opcao-geral');
     let icone = window.document.getElementById('config-icone');
     opcoes.forEach(function(opcoes){
@@ -21,11 +21,10 @@ function eventoEquiv(){
 }
 
 function configBody(){
-    let symbols = window.document.querySelectorAll('.material-symbols-outlined')
+    let symbols = window.document.querySelectorAll('.material-symbols-display')
     let opcoes = window.document.querySelectorAll('.estilo-opcao-geral')
     let icone = window.document.getElementById('config-icone')
     let equiv = window.document.getElementById('estilo-equiv')
-    let config_display = window.document.querySelectorAll('.config-display')
 
     if(window.innerWidth > 642){
         symbols.forEach(function(symbols){
@@ -46,7 +45,5 @@ function configBody(){
         icone.style.display = 'none';
         equiv.style.display = 'block';
     }
-    config_display.forEach(function(config_display){
-        config_display.style.display = 'block'
-    })
+
 }
