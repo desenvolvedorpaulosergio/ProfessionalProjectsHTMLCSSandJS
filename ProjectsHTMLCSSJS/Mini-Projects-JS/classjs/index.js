e9fe831f7d4c57a0b47e821166debf512d210791
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-11-18 15:04:46 -0300

    Atualizar

--- ProjectsHTMLCSSJS/Mini-Projects-JS/classjs/index.js
+++ ProjectsHTMLCSSJS/Mini-Projects-JS/classjs/index.js
@@ -9,7 +9,7 @@ function analizeLista(x,y){
 }
 
 function agrupar(){
-    let numero = window.document.getElementById('numero').value
+    let numero = Number(window.document.getElementById('numero').value)
     let listaDiv = window.document.getElementById('lista-div')
     if(numero >= 1 && numero <= 100 && analizeLista(lista, numero)){
         listaDiv.innerHTML += `${numero} <br>`
@@ -17,4 +17,23 @@ function agrupar(){
     }else{
         alert('preencha os dados corretamente')
     }
+}
+
+function relatorio(){
+    if(lista.length >= 2){
+        window.document.getElementById('total-elementos').innerHTML = `${lista.length}`
+        window.document.getElementById('maior-valor').innerHTML = `${Math.max(lista)}`
+        // window.document.getElementById('menor-valor').innerHTML = `${}`
+        // window.document.getElementById('media').innerHTML = `${}`
+        // window.document.getElementById('soma-total').innerHTML = `${}`
+        // window.document.getElementById('ordem-crescente').innerHTML = `${}`
+        // window.document.getElementById('ordem-drescente').innerHTML = `${}`
+        window.document.getElementById('relatorio-dados-display').style.display = 'block'
+    }else{
+        alert('é necessário ter pelo menos 2 números')
+    }
+}
+
+function voltar(){
+    window.document.getElementById('relatorio-dados-display').style.display = 'none'
 }
