88fbe3f6e7696696eaae16c40b4c233bc0ee459e
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-11-25 08:25:05 -0300

    Atualizar

--- ProjectsHTMLCSSJS/Mini-Projects-JS/classjs/config-calculadora.js
+++ ProjectsHTMLCSSJS/Mini-Projects-JS/classjs/config-calculadora.js
@@ -3,56 +3,38 @@ var caixaArray = []
 
 function evento1(){
     window.document.getElementById('caixa-entrada').innerHTML += 1
-    caixaEntrada.innerHTML = caixaArray.join(', ')
-    caixaArray.push(1);
 }
 
 function evento2(){
     window.document.getElementById('caixa-entrada').innerHTML += 2
-    caixaEntrada.innerHTML = caixaArray.join(', ')
-    caixaArray.push(2);
 }
 
 function evento3(){
     window.document.getElementById('caixa-entrada').innerHTML += 3
-    caixaEntrada.innerHTML = caixaArray.join(', ')
-    caixaArray.push(3);
 }
 
 function evento4(){
     window.document.getElementById('caixa-entrada').innerHTML += 4
-    caixaEntrada.innerHTML = caixaArray.join(', ')
-    caixaArray.push(4);
 }
 
 function evento5(){
     window.document.getElementById('caixa-entrada').innerHTML += 5
-    caixaEntrada.innerHTML = caixaArray.join(', ')
-    caixaArray.push(5);
 }
 
 function evento6(){
     window.document.getElementById('caixa-entrada').innerHTML += 6
-    caixaEntrada.innerHTML = caixaArray.join(', ')
-    caixaArray.push(6);
 }
 
 function evento7(){
     window.document.getElementById('caixa-entrada').innerHTML += 7
-    caixaEntrada.innerHTML = caixaArray.join(', ')
-    caixaArray.push(7);
 }
 
 function evento8(){
     window.document.getElementById('caixa-entrada').innerHTML += 8
-    caixaEntrada.innerHTML = caixaArray.join(', ')
-    caixaArray.push(8);
 }
 
 function evento9(){
     window.document.getElementById('caixa-entrada').innerHTML += 9
-    caixaEntrada.innerHTML = caixaArray.join(', ')
-    caixaArray.push(9);
 }
 
 function eventoDelete(){
@@ -60,3 +42,24 @@ function eventoDelete(){
     caixaEntrada = []
 }
 
+function eventoSoma(){
+    window.document.getElementById('caixa-entrada').innerHTML += "+"
+}
+
+function eventoMenos(){
+    window.document.getElementById('caixa-entrada').innerHTML += "-"
+}
+
+function eventoMult(){
+    window.document.getElementById('caixa-entrada').innerHTML += "*"
+}
+
+function eventoDiv(){
+    window.document.getElementById('caixa-entrada').innerHTML += "/"
+}
+
+//Nova função "eval(variável)": função que interpreta as operações entre asplas duplas e calcula.
+function eventoIgual(){
+    window.document.getElementById('caixa-entrada').innerHTML = eval(calculo)
+    let calculo = window.document.getElementById('caixa-entrada').innerHTML
+}
