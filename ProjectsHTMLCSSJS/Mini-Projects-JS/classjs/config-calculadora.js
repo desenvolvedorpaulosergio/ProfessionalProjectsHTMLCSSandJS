a2e09bd87393bdf02dcf889ccc7dbb0a99f7b6e7
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-11-25 07:32:19 -0300

    Atualizar

--- ProjectsHTMLCSSJS/Mini-Projects-JS/classjs/config-calculadora.js
+++ ProjectsHTMLCSSJS/Mini-Projects-JS/classjs/config-calculadora.js
@@ -6,3 +6,56 @@ function evento1(){
     caixaArray.push = 1;
 }
 
+function evento2(){
+    var caixaEntrada = window.document.getElementById('caixa-entrada')
+    caixaEntrada.innerHTML += 2;
+    caixaArray.push = 2;
+}
+
+function evento3(){
+    var caixaEntrada = window.document.getElementById('caixa-entrada')
+    caixaEntrada.innerHTML += 3;
+    caixaArray.push = 3;
+}
+
+function evento4(){
+    var caixaEntrada = window.document.getElementById('caixa-entrada')
+    caixaEntrada.innerHTML += 4;
+    caixaArray.push = 4;
+}
+
+function evento5(){
+    var caixaEntrada = window.document.getElementById('caixa-entrada')
+    caixaEntrada.innerHTML += 5;
+    caixaArray.push = 5;
+}
+
+function evento6(){
+    var caixaEntrada = window.document.getElementById('caixa-entrada')
+    caixaEntrada.innerHTML += 6;
+    caixaArray.push = 6;
+}
+
+function evento7(){
+    var caixaEntrada = window.document.getElementById('caixa-entrada')
+    caixaEntrada.innerHTML += 7;
+    caixaArray.push = 7;
+}
+
+function evento8(){
+    var caixaEntrada = window.document.getElementById('caixa-entrada')
+    caixaEntrada.innerHTML += 8;
+    caixaArray.push = 8;
+}
+
+function evento9(){
+    var caixaEntrada = window.document.getElementById('caixa-entrada')
+    caixaEntrada.innerHTML += 9;
+    caixaArray.push = 9;
+}
+
+function evento0(){
+    var caixaEntrada = window.document.getElementById('caixa-entrada')
+    caixaEntrada.innerHTML += 0;
+    caixaArray.push = 0;
+}
