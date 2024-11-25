145f660d83c1cbec7e94709597f13e2188ca8496
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-11-25 08:07:34 -0300

    Atualizar

--- ProjectsHTMLCSSJS/Mini-Projects-JS/classjs/config-calculadora.js
+++ ProjectsHTMLCSSJS/Mini-Projects-JS/classjs/config-calculadora.js
@@ -1,61 +1,62 @@
+var caixaEntrada = window.document.getElementById('caixa-entrada')
 var caixaArray = []
 
 function evento1(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 1;
-    caixaArray.push = 1;
+    window.document.getElementById('caixa-entrada').innerHTML += 1
+    caixaEntrada.innerHTML = caixaArray.join(', ')
+    caixaArray.push(1);
 }
 
 function evento2(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 2;
-    caixaArray.push = 2;
+    window.document.getElementById('caixa-entrada').innerHTML += 2
+    caixaEntrada.innerHTML = caixaArray.join(', ')
+    caixaArray.push(2);
 }
 
 function evento3(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 3;
-    caixaArray.push = 3;
+    window.document.getElementById('caixa-entrada').innerHTML += 3
+    caixaEntrada.innerHTML = caixaArray.join(', ')
+    caixaArray.push(3);
 }
 
 function evento4(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 4;
-    caixaArray.push = 4;
+    window.document.getElementById('caixa-entrada').innerHTML += 4
+    caixaEntrada.innerHTML = caixaArray.join(', ')
+    caixaArray.push(4);
 }
 
 function evento5(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 5;
-    caixaArray.push = 5;
+    window.document.getElementById('caixa-entrada').innerHTML += 5
+    caixaEntrada.innerHTML = caixaArray.join(', ')
+    caixaArray.push(5);
 }
 
 function evento6(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 6;
-    caixaArray.push = 6;
+    window.document.getElementById('caixa-entrada').innerHTML += 6
+    caixaEntrada.innerHTML = caixaArray.join(', ')
+    caixaArray.push(6);
 }
 
 function evento7(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 7;
-    caixaArray.push = 7;
+    window.document.getElementById('caixa-entrada').innerHTML += 7
+    caixaEntrada.innerHTML = caixaArray.join(', ')
+    caixaArray.push(7);
 }
 
 function evento8(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 8;
-    caixaArray.push = 8;
+    window.document.getElementById('caixa-entrada').innerHTML += 8
+    caixaEntrada.innerHTML = caixaArray.join(', ')
+    caixaArray.push(8);
 }
 
 function evento9(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 9;
-    caixaArray.push = 9;
+    window.document.getElementById('caixa-entrada').innerHTML += 9
+    caixaEntrada.innerHTML = caixaArray.join(', ')
+    caixaArray.push(9);
 }
 
-function evento0(){
-    var caixaEntrada = window.document.getElementById('caixa-entrada')
-    caixaEntrada.innerHTML += 0;
-    caixaArray.push = 0;
+function eventoDelete(){
+    window.document.getElementById('caixa-entrada').innerHTML = ""
+    caixaEntrada = []
 }
+
