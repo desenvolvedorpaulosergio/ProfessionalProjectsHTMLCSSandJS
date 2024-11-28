23fb20e8eb08100ed06e02bd1d6af33ea03f5620
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2024-11-28 15:15:52 -0300

    Atualizar

---
+++ ProjectsHTMLCSSJS/ThematicSystems/CocaCola/classjs/index.js
@@ -0,0 +1,13 @@
+function tamanhoBody(){
+    if(window.innerWidth > 570){
+        
+    }
+}
+
+function eventoEquiv(){
+    if(window.document.getElementById('container-estilo-opcoes').style.display == 'block'){
+        window.document.getElementById('container-estilo-opcoes').style.display = 'none'
+    }else{
+        window.document.getElementById('container-estilo-opcoes').style.display = 'block'
+    }
+}
