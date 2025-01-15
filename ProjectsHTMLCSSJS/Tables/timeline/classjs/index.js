aa432db3ec773e4585f71898d85d30c26b85c4cf
Paulo Sérgio da Silva Marques <165834844+desenvolvedorpaulosergio@users.noreply.github.com>
2025-01-15 08:46:41 -0300

    enviar

--- ProjectsHTMLCSSJS/Tables/timeline/classjs/index.js
+++ ProjectsHTMLCSSJS/Tables/timeline/classjs/index.js
@@ -2,3 +2,11 @@ window.document.addEventListener('DOMContentLoaded', function(){
     let data = new Date().getFullYear()
     document.getElementById('config-estilo2').innerHTML = `${data}`
 })
+
+function eventoInprimir(){
+    window.print()
+}
+
+function eventoAdd(){
+    
+}
